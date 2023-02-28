98b2557129fc61e0753a93b1c57700bb6f580717
Anish <96220428+Anishsuresh3@users.noreply.github.com>
2023-02-28 23:12:27 +0530

    Add files via upload

--- FireBase/lib/app_screens/player_list.dart
+++ FireBase/lib/app_screens/player_list.dart
@@ -1,66 +1,24 @@
 import 'package:flutter/material.dart';
+import '../models/gamer.dart';
 
-class player_list extends StatefulWidget{
-  @override
-  State<StatefulWidget> createState() {
-    return players();
-  }
-}
-class players extends State<player_list>{
+class playerTile extends StatelessWidget {
+  final Gamer gamer;
+  playerTile({required this.gamer});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Skadoodle!"),
-      ),
-      body: getLongListView(),
+    return Padding(
+        padding: EdgeInsets.only(top: 10.0),
+        child: Card(
+          margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              radius: 25.0,
+            ),
+            title: Text(gamer.name),
+            subtitle: Text(gamer.points.toString()),
+            trailing: Icon(Icons.add_box_outlined),
+          ),
+        ),
     );
-  }
-  Widget getLongListView(){
-    ListPlayers t1 = ListPlayers();
-    t1.add('Anish', 355, 1);
-    t1.add('Rahul',245,2);
-    var listview = ListView.builder(
-        itemBuilder: (context,index){
-          return ListTile(
-            leading: Icon(Icons.face),
-            title: Text("Person"),
-            subtitle: Text("call"),
-            trailing: Icon(Icons.phone),
-          );
-        }
-    );
-    return listview;
-  }
-}
-class Node{
-  String name='';
-  dynamic points=0;
-  int rank=0;
-  Node? next;
-  Node? prev;
-  Node(this.name,this.points,this.rank){
-    this.next=null;
-    this.prev=null;
-  }
-}
-class ListPlayers{
-  Node? _head;
-  Node? _tail;
-  ListPlayers(){
-    _head=null;
-    _tail=null;
-  }
-  bool get isEmpty => _head==null;
-  void add(String name,dynamic points,int rank){
-    Node? t= Node(name,points,rank);
-    if(isEmpty){
-      _head = _tail = t;
-      _head!.next = _head!.prev;
-    }
-    _tail!.next = t;
-    t.prev = _tail;
-    t.next = _head;
-    _tail = _tail!.next;
   }
 }
